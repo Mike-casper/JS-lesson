e9d2039916c8a5ab6d63cd20527ba2908b792377
Mike-casper <misha7709123@gmail.com>
2022-10-19 22:15:32 +0300

    done project

--- second.js
+++ second.js
@@ -27,37 +27,72 @@ let students = [
 // const names = ["Bob","Alex","Nick","John"]
 
 
-const getNames = (array) =>{
-    const result = []
-    for(let i=0;i<array.length;i++){
-        result[i] = array[i].name
+const funcName =(x)=>x.name
+const funcScore=(x)=>x.scores
+const funcStudent=(x)=>({...x, isStudent:true})
+
+// const globalM=(array,func)=>{
+//     const result=[]
+//     for (let i=0; i<array.length;i++){
+//         result[i] = func(array[i])
+//     }
+//     return result
+// }
+// console.log(global(students,funcName))
+
+console.log(students.map(funcName))
+//
+// console.log(global(students,funcScore))
+// console.log(global(students,funcStudent))
+
+const globalF = (array, func) =>{
+    const result=[]
+    for (let i=0; i<array.length;i++){
+        if(func(array[i])===true){ //true || false
+            result.push(array[i])
+        }
     }
     return result
 }
-console.log(getNames(students))
 
-//const scores = [85,89,120,100]
+console.log(globalF(students, s=>s.age>20))
+console.log(students.filter( s=>s.age>20))
 
-const getScores = (array)=>{
-    const result = []
-    for(let i=0;i<array.length;i++){
-        result[i] = array[i].scores
+const globalFD = (array,func)=>{
+    for (let i=0; i<array.length;i++){
+        if(func(array[i])===true){ //true || false
+            return array[i]
+        }
     }
-    return result
 }
 
-const getStudents = (array)=>{
-    const func = (x) =>({...x, isStudent: true})
+console.log(globalFD(students, s=>s.name==="Bob"))
+console.log(students.find(s=>s.name==="Bob"))
 
-    const result = []
-    for(let i=0;i<array.length;i++){
-        result[i] = func(array[i])
-    }
-    return result
+const globalPush = (array, el) => {
+    // return [...array,el]
+    array[array.length]=el;
+    return array.length
+
 }
 
-console.log(getStudents(students))
+console.log((students))
+console.log(globalPush(students, {hey:"Yo"}))
+console.log((students))
 
-const global = (array, func)=>{
+const numberss = [1,2,3,4,5]
 
+const  globalIndexOf = (numberss.indexOf(2))// 1
+console.log(globalIndexOf)
+
+const globalIndexOff = (numberss.indexOf(6))// -1
+console.log(globalIndexOff)
+
+const globalIndex = (arr, el) => {
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] === el) {
+            return i
+        }
+    }
+    return -1
 }
